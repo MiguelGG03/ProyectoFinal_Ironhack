a4b18720c9fc377cc39ce63a8e4ddd7eb09939ba
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2023-11-26 19:15:27 +0100

    todo el DOM que se necesita

--- main.js
+++ main.js
@@ -9,4 +9,6 @@ const empezarBoton = document.querySelector('.empezar-boton');
 const plantilla = document.querySelector('#plantilla-casilla');
 const fragmento = document.createDocumentFragment();
 const tabla = document.querySelector('.tabla');
-const elementoPuntuacion = document.querySelector('.puntuacion');
+const elementoPuntuacion = document.querySelector('.elemento__tabla-puntuacion');
+const elementoTiempo = document.querySelector('.elemento__tabla-tiempo');
+const finalizarVisualizacion = document.querySelector('.finalizar-visualizacion');
