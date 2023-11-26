fcf707b24321d949c810d12eafbe85dee7e53b89
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2023-11-26 19:10:48 +0100

    AÑADIENDO ELEMENTOS DEL DOM

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 import {emojiBanders} from './emojiBanderas.js';
 import {mezclarArray} from '-/extras/mezclarArray.js';
+
+/*
+SELECTORES DOM
+*/
+
+const empezarBoton = document.querySelector('.empezar-boton');
+const plantilla = document.querySelector('#plantilla-casilla');
+const fragmento = document.createDocumentFragment();
+const tabla = document.querySelector('.tabla');
+const elementoPuntuacion = document.querySelector('.puntuacion');
