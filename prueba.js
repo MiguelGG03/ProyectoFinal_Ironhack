691a0ca41deef8b998baaf7d1810d1080253afc7
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2023-11-27 10:38:11 +0100

    no logro que funciionen las cosas

--- prueba.js
+++ prueba.js
@@ -1,2 +1,13 @@
 import {mezclarArray} from './extras/mezclarArray.js'
 import {emojiBanderas} from './emojiBanderas.js'
+
+/*
+* SELECTORES DEL DOM
+*/
+
+const empezarBtn = document.querySelector('.empezar-boton')
+empezarBtn.addEventListener('click', empezarJuego)
+
+function empezarJuego() {
+    console.log('Empezar');
+}
